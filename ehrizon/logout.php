edb297907e560b39ef62f5b0f2bd62c102b195d3
Sharmila-1912 <rsharmila088@gmail.com>
2024-06-29 15:54:53 +0530

    Add files via upload

---
+++ ehrizon/logout.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Red Text</title>
+    <link rel="stylesheet" href="style3.css">
+</head>
+<body>
+    <p class="red-text">YOU LOGOUT SUCCESSFULLY....!!!!</p>
+</body>
+</html>
